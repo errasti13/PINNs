1377c57bac5d549a68f463b16e9437efb66071e5
Jon Errasti <jon44errasti@gmail.com>
2024-06-11 21:12:59 +0200

    Moved heat and burgers equations to a separate folder

--- problem/Burgers.py
+++ problem/Burgers.py
@@ -1,34 +1,35 @@
 import numpy as np
 import tensorflow as tf
 
+from problem.boundaryCondition import *
+
 class BurgersEquation:
     def __init__(self, nu=0.01 / np.pi):
         self.nu = nu
+        self.bc_library = BoundaryConditionLibrary()
+
 
     def getInitialSolution(self, N0, x_min, x_max, t_min, sampling_method):
         if sampling_method == 'random':
-            x0 = (np.random.rand(N0, 1) * (x_max - x_min) + x_min).astype(np.float32)  # Initial condition: x in [x_min, x_max]
+            x0 = (np.random.rand(N0, 1) * (x_max - x_min) + x_min).astype(np.float32)
         elif sampling_method == 'uniform':
-            x0 = np.linspace(x_min, x_max, N0)[:, None].astype(np.float32)  # Uniform sampling: x in [x_min, x_max]
+            x0 = np.linspace(x_min, x_max, N0)[:, None].astype(np.float32)
         
-        t0 = np.full((N0, 1), t_min, dtype=np.float32)  # Initial condition: t = t_min
-        u0 = -np.sin(2 * np.pi * x0 / (x_max - x_min)).astype(np.float32)  # Initial velocity
+        t0 = np.full((N0, 1), t_min, dtype=np.float32) 
+        u0 = -np.sin(2 * np.pi * x0 / (x_max - x_min)).astype(np.float32) 
 
         return x0, t0, u0
     
-
-    def getBoundaryCondition(self, N0, t_min, t_max, x, sampling_method='uniform'):
+    def getBoundaryCondition(self, N0, t_min, t_max, x, sampling_method='uniform', boundaryCondition='zeros'):
         # Generate time coordinates based on the sampling method
         if sampling_method == 'random':
-            tBc = (np.random.rand(N0, 1) * (t_max - t_min) + t_min).astype(np.float32)  # Random sampling in [t_min, t_max]
+            tBc = (np.random.rand(N0, 1) * (t_max - t_min) + t_min).astype(np.float32)
         elif sampling_method == 'uniform':
-            tBc = np.linspace(t_min, t_max, N0)[:, None].astype(np.float32)  # Uniform sampling in [t_min, t_max]
+            tBc = np.linspace(t_min, t_max, N0)[:, None].astype(np.float32)
         else:
             raise ValueError("sampling_method should be 'random' or 'uniform'")
-
-        xBc = np.full((N0, 1), x, dtype=np.float32)  # Boundary x-coordinate is constant
-
-        uBc = np.zeros((N0, 1), dtype=np.float32)  # Initial velocity/condition
+        
+        xBc, uBc = self.bc_library.getBoundaryCondition(N0, x, boundaryCondition=boundaryCondition)
 
         return xBc, tBc, uBc
 
@@ -38,11 +39,11 @@ class BurgersEquation:
 
         x0, t0, u0 = self.getInitialSolution(N0, x_min, x_max, t_min, sampling_method)
 
-        xBc0, tBc0, uBc0 = self.getBoundaryCondition(N0, t_min, t_max, x_min, sampling_method)
-        xBc1, tBc1, uBc1 = self.getBoundaryCondition(N0, t_min, t_max, x_max, sampling_method)
+        xBc0, tBc0, uBc0 = self.getBoundaryCondition(N0, t_min, t_max, x_min, sampling_method, boundaryCondition='zeros')
+        xBc1, tBc1, uBc1 = self.getBoundaryCondition(N0, t_min, t_max, x_max, sampling_method, boundaryCondition='zeros')
 
-        x_f = (np.random.rand(Nf, 1) * (x_max - x_min) + x_min).astype(np.float32)  # Collocation points: x in [x_min, x_max]
-        t_f = (np.random.rand(Nf, 1) * (t_max - t_min) + t_min).astype(np.float32)  # Collocation points: t in [t_min, t_max]
+        x_f = (np.random.rand(Nf, 1) * (x_max - x_min) + x_min).astype(np.float32)
+        t_f = (np.random.rand(Nf, 1) * (t_max - t_min) + t_min).astype(np.float32)
 
         return x_f, t_f, u0, x0, t0, xBc0, tBc0, uBc0, xBc1, tBc1, uBc1 
 
@@ -111,6 +112,9 @@ class BurgersEquation:
 
         # Store initial condition
         u_solution[0, :] = u_initial
+        
+        _, _, uBc1 = self.getBoundaryCondition(Nt, t_min, t_max, x_min, boundaryCondition='zeros')
+        _, _, uBc2 = self.getBoundaryCondition(Nt, t_min, t_max, x_max, boundaryCondition='zeros')
 
         # Time-stepping loop
         for n in range(1, Nt + 1):
@@ -120,8 +124,8 @@ class BurgersEquation:
                             - dt * u[1:Nx-1] * (u[1:Nx-1] - u[0:Nx-2]) / dx 
                             + alpha * (u[2:Nx] - 2 * u[1:Nx-1] + u[0:Nx-2]))
 
-            u_new[0] = 0.0
-            u_new[-1] = 0.0
+            u_new[0] = uBc1[n - 1]
+            u_new[-1] = uBc2[n - 1]
 
             u = u_new.copy()
         
