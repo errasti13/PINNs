4087abf5aafddd5dffb05874484091bf921aaa76
Jon Errasti <jon44errasti@gmail.com>
2024-10-04 07:29:21 +0200

    Generalized boundary condition imposition.

--- problem/NavierStokes.py
+++ problem/NavierStokes.py
@@ -28,25 +28,34 @@ class SteadyNavierStokes2D:
 
         return x_f, y_f, xBc_left, yBc_left, uBc_left, vBc_left, xBc_right, yBc_right, uBc_right, vBc_right, \
                xBc_bottom, yBc_bottom, uBc_bottom, vBc_bottom, xBc_top, yBc_top, uBc_top, vBc_top
+    
+    def imposeBoundaryCondition(self, uBc, vBc):
+        if uBc is not None and vBc is not None:
+            uBc = tf.convert_to_tensor(uBc, dtype=tf.float32)
+            vBc = tf.convert_to_tensor(vBc, dtype=tf.float32)
 
+        return uBc, vBc
+    
+    def computeBoundaryLoss(self, model, xBc, yBc, uBc, vBc):
+        if uBc is not None and vBc is not None:
+            uPred = model(tf.concat([tf.cast(xBc, dtype=tf.float32), tf.cast(yBc, dtype=tf.float32)], axis=1))[:, 0]
+            vPred = model(tf.concat([tf.cast(xBc, dtype=tf.float32), tf.cast(yBc, dtype=tf.float32)], axis=1))[:, 1]
+
+            return tf.reduce_mean(tf.square(uPred - uBc)), tf.reduce_mean(tf.square(vPred - vBc))
+        
+        else:
+            return tf.constant(0.0)
+        
     def loss_function(self, model, data):
         # Unpack the data
         x_f, y_f, xBc_left, yBc_left, uBc_left, vBc_left, xBc_right, yBc_right, uBc_right, vBc_right, \
         xBc_bottom, yBc_bottom, uBc_bottom, vBc_bottom, xBc_top, yBc_top, uBc_top, vBc_top = data
 
         # Convert boundary data to tensors
-        x_f = tf.convert_to_tensor(x_f, dtype=tf.float32)
-        y_f = tf.convert_to_tensor(y_f, dtype=tf.float32)
-        
-        # Convert boundary conditions into tensors
-        uBc_left = tf.convert_to_tensor(uBc_left, dtype=tf.float32)
-        vBc_left = tf.convert_to_tensor(vBc_left, dtype=tf.float32)
-        uBc_right = tf.convert_to_tensor(uBc_right, dtype=tf.float32)
-        vBc_right = tf.convert_to_tensor(vBc_right, dtype=tf.float32)
-        uBc_bottom = tf.convert_to_tensor(uBc_bottom, dtype=tf.float32)
-        vBc_bottom = tf.convert_to_tensor(vBc_bottom, dtype=tf.float32)
-        uBc_top = tf.convert_to_tensor(uBc_top, dtype=tf.float32)
-        vBc_top = tf.convert_to_tensor(vBc_top, dtype=tf.float32)
+        uBc_left, vBc_left = self.imposeBoundaryCondition(uBc_left, vBc_left)
+        uBc_right, vBc_right = self.imposeBoundaryCondition(uBc_right, vBc_right)
+        uBc_top, vBc_top = self.imposeBoundaryCondition(uBc_top, vBc_top)
+        uBc_bottom, vBc_bottom = self.imposeBoundaryCondition(uBc_bottom, vBc_bottom)
 
         with tf.GradientTape(persistent=True) as tape:
             tape.watch([x_f, y_f])
@@ -85,43 +94,20 @@ class SteadyNavierStokes2D:
         f_loss_v = tf.reduce_mean(tf.square(momentum_v))
         continuity_loss = tf.reduce_mean(tf.square(continuity))
 
-        # Boundary condition losses
-        u_left_pred = model(tf.concat([tf.cast(xBc_left, dtype=tf.float32), 
-                                    tf.cast(yBc_left, dtype=tf.float32)], axis=1))[:, 0]
-        v_left_pred = model(tf.concat([tf.cast(xBc_left, dtype=tf.float32), 
-                                    tf.cast(yBc_left, dtype=tf.float32)], axis=1))[:, 1]
+        uBc_loss_left, vBc_loss_left = self.computeBoundaryLoss(model, xBc_left, yBc_left, uBc_left, vBc_left)
+        uBc_loss_right, vBc_loss_right = self.computeBoundaryLoss(model, xBc_right, yBc_right, uBc_right, vBc_right)
+        uBc_loss_top, vBc_loss_top = self.computeBoundaryLoss(model, xBc_top, yBc_top, uBc_top, vBc_top)
+        uBc_loss_bottom, vBc_loss_bottom = self.computeBoundaryLoss(model, xBc_bottom, yBc_bottom, uBc_bottom, vBc_bottom)
 
-        u_right_pred = model(tf.concat([tf.cast(xBc_right, dtype=tf.float32), 
-                                        tf.cast(yBc_right, dtype=tf.float32)], axis=1))[:, 0]
-        v_right_pred = model(tf.concat([tf.cast(xBc_right, dtype=tf.float32), 
-                                        tf.cast(yBc_right, dtype=tf.float32)], axis=1))[:, 1]
-        
-        u_top_pred = model(tf.concat([tf.cast(xBc_top, dtype=tf.float32), 
-                                    tf.cast(yBc_top, dtype=tf.float32)], axis=1))[:, 0]
-        v_top_pred = model(tf.concat([tf.cast(xBc_top, dtype=tf.float32), 
-                                    tf.cast(yBc_top, dtype=tf.float32)], axis=1))[:, 1]
-
-        u_bottom_pred = model(tf.concat([tf.cast(xBc_bottom, dtype=tf.float32), 
-                                        tf.cast(yBc_bottom, dtype=tf.float32)], axis=1))[:, 0]
-        v_bottom_pred = model(tf.concat([tf.cast(xBc_bottom, dtype=tf.float32), 
-                                        tf.cast(yBc_bottom, dtype=tf.float32)], axis=1))[:, 1]
-
-        # Calculate the L2 loss for boundary conditions
-        uBc_loss_left = tf.reduce_mean(tf.square(u_left_pred - uBc_left))
-        vBc_loss_left = tf.reduce_mean(tf.square(v_left_pred - vBc_left))
-        uBc_loss_right = tf.reduce_mean(tf.square(u_right_pred - uBc_right))
-        vBc_loss_right = tf.reduce_mean(tf.square(v_right_pred - vBc_right))
-        uBc_loss_top = tf.reduce_mean(tf.square(u_top_pred - uBc_top))
-        vBc_loss_top = tf.reduce_mean(tf.square(v_top_pred - vBc_top))
-        uBc_loss_bottom = tf.reduce_mean(tf.square(u_bottom_pred - uBc_bottom))
-        vBc_loss_bottom = tf.reduce_mean(tf.square(v_bottom_pred - vBc_bottom))
-        
         # Total loss
-        total_loss = (f_loss_u + f_loss_v + continuity_loss +
-                    uBc_loss_left + vBc_loss_left + uBc_loss_right + vBc_loss_right + 
-                    uBc_loss_top + vBc_loss_top + uBc_loss_bottom + vBc_loss_bottom)
-        
+        total_loss = (f_loss_u + f_loss_v + continuity_loss + 
+                        uBc_loss_left + vBc_loss_left +
+                    uBc_loss_right + vBc_loss_right +
+                    uBc_loss_top + vBc_loss_top +
+                    uBc_loss_bottom + vBc_loss_bottom)
+
         return total_loss
+
 
     def predict(self, pinn, x_range, y_range, Nx=256, Ny=256):
         x_pred = np.linspace(x_range[0], x_range[1], Nx)[:, None].astype(np.float32)
@@ -192,11 +178,65 @@ class LidDrivenCavity(SteadyNavierStokes2D):
         vBc_top = np.zeros_like(yBc_top, dtype=np.float32)
 
         return xBc_left, yBc_left, uBc_left, vBc_left, xBc_right, yBc_right, uBc_right, vBc_right, xBc_bottom, yBc_bottom, uBc_bottom, vBc_bottom, xBc_top, yBc_top, uBc_top, vBc_top
+    
 
-
-# Another example problem, like ChannelFlow
 class ChannelFlow(SteadyNavierStokes2D):
 
-    def getBoundaryCondition(self, N0, x_min, x_max, y_min, y_max, sampling_method='uniform'):
-        # Channel flow specific boundary conditions
-        pass
+    def getBoundaryCondition(self, N0, x_min, x_max, y_min, y_max, 
+                             sampling_method='uniform', enforce_outlet=True):
+        if sampling_method == 'random':
+            xBc_left = np.full((N0, 1), x_min, dtype=np.float32)
+            yBc_left = np.random.rand(N0, 1) * (y_max - y_min) + y_min
+
+            xBc_right = np.full((N0, 1), x_max, dtype=np.float32)
+            yBc_right = np.random.rand(N0, 1) * (y_max - y_min) + y_min
+
+            yBc_bottom = np.full((N0, 1), y_min, dtype=np.float32)
+            xBc_bottom = np.random.rand(N0, 1) * (x_max - x_min) + x_min
+
+            yBc_top = np.full((N0, 1), y_max, dtype=np.float32)
+            xBc_top = np.random.rand(N0, 1) * (x_max - x_min) + x_min
+        elif sampling_method == 'uniform':
+            yBc = np.linspace(y_min, y_max, N0)[:, None].astype(np.float32)
+            xBc = np.linspace(x_min, x_max, N0)[:, None].astype(np.float32)
+
+            xBc_left = np.full_like(yBc, x_min, dtype=np.float32)
+            yBc_left = yBc
+
+            xBc_right = np.full_like(yBc, x_max, dtype=np.float32)
+            yBc_right = yBc
+
+            yBc_bottom = np.full_like(xBc, y_min, dtype=np.float32)
+            xBc_bottom = xBc
+
+            yBc_top = np.full_like(xBc, y_max, dtype=np.float32)
+            xBc_top = xBc
+
+        else:
+            raise ValueError("sampling_method should be 'random' or 'uniform'")
+
+        # Boundary conditions for u, v
+        uBc_left = np.ones_like(yBc_left, dtype=np.float32) * (1 - (yBc_left / y_max) ** 2)  # Parabolic inlet
+        vBc_left = np.zeros_like(yBc_left, dtype=np.float32)  # No vertical velocity at inlet
+
+        if enforce_outlet:
+            # Outlet: no boundary conditions (do-nothing approach)
+            uBc_right = np.zeros_like(xBc_right, dtype=np.float32)
+            vBc_right = np.zeros_like(xBc_right, dtype=np.float32)
+        else:
+            # No boundary condition at the outlet (do nothing)
+            uBc_right = None
+            vBc_right = None
+
+        # No-slip conditions for top and bottom
+        uBc_bottom = np.zeros_like(xBc_bottom, dtype=np.float32)
+        vBc_bottom = np.zeros_like(xBc_bottom, dtype=np.float32)
+        
+        uBc_top = np.ones_like(xBc_top, dtype=np.float32)
+        vBc_top = np.zeros_like(xBc_top, dtype=np.float32)
+
+        return (xBc_left, yBc_left, uBc_left, vBc_left,
+                xBc_right, yBc_right, uBc_right, vBc_right,
+                xBc_bottom, yBc_bottom, uBc_bottom, vBc_bottom,
+                xBc_top, yBc_top, uBc_top, vBc_top)
+
